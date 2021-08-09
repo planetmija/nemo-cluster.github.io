f1eccf0d06e94a7a399521df09bbd81421ee7d6a
Michael Janczyk <190198+planetmija@users.noreply.github.com>
2021-08-09 18:12:14 +0200

    assets: js: nemo_stat: move nemo_options.js to top and add nemo_cores_free.js.

--- assets/js/nemo_stat.js
+++ assets/js/nemo_stat.js
@@ -4,6 +4,7 @@ $.ajax({
     statusCode: {
         200: function (response) {
             $('#statinfo').remove();
+            $.getScript("/assets/js/highcharts/nemo_options.js");
             $.getScript("/assets/js/highcharts/nemo_today_usage.js");
             $.getScript("/assets/js/highcharts/nemo_day_usage.js");
             $.getScript("/assets/js/highcharts/nemo_site_usage.js");
@@ -12,7 +13,7 @@ $.ajax({
             $.getScript("/assets/js/highcharts/nemo_rv_usage.js");
             $.getScript("/assets/js/highcharts/nemo_rv_lastyear.js");
             $.getScript("/assets/js/highcharts/nemo_rv_top10.js");
-            $.getScript("/assets/js/highcharts/nemo_options.js");
+            $.getScript("/assets/js/highcharts/nemo_cores_free.js");
             $.getScript("/assets/js/nemo_hide_chart_button.js");
         },
         404: function (response) {
